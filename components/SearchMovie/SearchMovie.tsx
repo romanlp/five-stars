72ef68e1db5d4015dee0df55a98937652e0942a4
romanlp <roman.lapacherie@gmail.com>
2022-12-01 15:13:10 +0000

    debounce search

--- components/SearchMovie/SearchMovie.tsx
+++ components/SearchMovie/SearchMovie.tsx
@@ -1,37 +1,41 @@
 import { Autocomplete, AutocompleteItem, Loader } from "@mantine/core";
-import { useState } from "react";
-import { searchMovies } from "../../lib/tmdb.api";
+import { useDebouncedValue } from "@mantine/hooks";
 import Router from "next/router";
+import { useEffect, useState } from "react";
+
+import { searchMovies } from "../../lib/tmdb.api";
 
 export default function SearchMovie() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
+  const [debounced] = useDebouncedValue(value, 300);
+
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<AutocompleteItem[]>([]);
+
+  useEffect(() => {
+    setData([]);
+
+    if (debounced.trim().length === 0) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+      searchMovies(debounced).then((response) => {
+        setLoading(false);
+        setData(response.results.map(movie => ({ value: movie.title, id: movie.id })));
+      });
+    }
+  }, [debounced]);
 
   const onItemSubmit = (item: AutocompleteItem) => {
     Router.push(`/movie/${item.id}`)
   }
 
-  const handleChange = (val: string) => {
-    setValue(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes("@")) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      searchMovies(val).then((response) => {
-        setLoading(false);
-        setData(response.results.map(movie => ({ value: movie.title, id: movie.id })));
-      });
-    }
-  };
   return (
 
     <Autocomplete
       value={value}
       data={data}
-      onChange={handleChange}
+      onChange={setValue}
       onItemSubmit={onItemSubmit}
       limit={10}
       rightSection={<Loader visibility={loading ? 'visible' : 'hidden'} size={16}/>}
