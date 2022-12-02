f64d3ab09f8144ff0799476ca555859c51761ec8
romanlp <roman.lapacherie@gmail.com>
2022-12-02 11:09:02 +0000

    save movie to firestore when it is not saved yet

--- lib/tmdb.api.ts
+++ lib/tmdb.api.ts
@@ -1,4 +1,4 @@
-import { MovieSummary } from "./interfaces/movie.type";
+import { Movie, MovieSummary } from "./interfaces";
 
 const baseUrl = `https://api.themoviedb.org/3`;
 const apiKey = `api_key=${process.env.NEXT_PUBLIC_MOVIEDB_API_KEY}`;
@@ -6,4 +6,9 @@ const apiKey = `api_key=${process.env.NEXT_PUBLIC_MOVIEDB_API_KEY}`;
 export const getDiscoverMovies = async () => {
   const res = await fetch(`${baseUrl}/discover/movie?${apiKey}`);
   return await res.json() as MovieSummary[]
+}
+
+export const getMovie = async (id: string) => {
+  const res = await fetch(`${baseUrl}/movie/${id}?${apiKey}`);
+  return await res.json() as Movie;
 }
--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -2,56 +2,58 @@ import Image, { ImageLoader } from 'next/image';
 import Link from "next/link";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+
 import Ratings from "../../components/ratings/Ratings";
+import { getMovie } from "../../lib/firebase.db";
 import styles from "./Movie.module.css";
 
 
-const fetcher = (url) => fetch(url).then(res => res.json());
+const fetcher = (id) => getMovie(id);
 
 function MoviePage() {
-    const router = useRouter()
-    const { id } = router.query
-    const { data, error } = useSWR(`https://api.themoviedb.org/3/movie/${id}?api_key=` + process.env.NEXT_PUBLIC_MOVIEDB_API_KEY, fetcher);
+  const router = useRouter()
+  const { id } = router.query
+  const { data, error } = useSWR(id, fetcher);
 
-    if (!data) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+  if (!data) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-    return <div>
-        <section className={styles.movieContainer}>
-            <h1 id={styles.h1Grid}>{data.title}</h1>
-            <div className={styles.moviePoster} id={styles.imgGrid}>
-                <Image loader={tmdbLoader} src={data.poster_path} alt={data.title} width={300} height={450} />
-            </div>
-            <div id={styles.plotGrid}>
-                <h2 className={styles.listItem}>Plot</h2>
-                <p >{data.overview}</p>
-            </div>
+  return <div>
+    <section className={styles.movieContainer}>
+      <h1 id={styles.h1Grid}>{data.title}</h1>
+      <div className={styles.moviePoster} id={styles.imgGrid}>
+        <Image loader={tmdbLoader} src={data.poster_path} alt={data.title} width={300} height={450}/>
+      </div>
+      <div id={styles.plotGrid}>
+        <h2 className={styles.listItem}>Plot</h2>
+        <p>{data.overview}</p>
+      </div>
 
-            <div id={styles.genreGrid}>
-                <h2 className={styles.listItem}>Genre</h2>
-                <p>{data.genres.map(e => e.name).join(', ')}</p>
-            </div>
+      <div id={styles.genreGrid}>
+        <h2 className={styles.listItem}>Genre</h2>
+        <p>{data.genres.map(e => e.name).join(', ')}</p>
+      </div>
 
-            <div id={styles.productionGrid}>
-                <h2 className={styles.listItem}>Production Companies</h2>
-                <p>{data.production_companies.map(e => e.name).join(', ')}</p>
-            </div>
+      <div id={styles.productionGrid}>
+        <h2 className={styles.listItem}>Production Companies</h2>
+        <p>{data.production_companies.map(e => e.name).join(', ')}</p>
+      </div>
 
-            <section className={styles.ratingContainer} id={styles.ratingGrid}>
-                <p>Rate the movie</p>
-                <Ratings movie={data} />
-            </section>
-            <div>
-                <Link href={'/'} passHref>
-                    <button>Go Back</button>
-                </Link>
-            </div>
-        </section>
-    </div>
+      <section className={styles.ratingContainer} id={styles.ratingGrid}>
+        <p>Rate the movie</p>
+        <Ratings movie={data}/>
+      </section>
+      <div>
+        <Link href={'/'} passHref>
+          <button>Go Back</button>
+        </Link>
+      </div>
+    </section>
+  </div>
 }
 
 const tmdbLoader: ImageLoader = ({ src, width, quality }) => {
-    return `https://image.tmdb.org/t/p/w500/${src}?w=${width}&q=${quality || 75}`
+  return `https://image.tmdb.org/t/p/w500/${src}?w=${width}&q=${quality || 75}`
 }
 
 export default MoviePage;
