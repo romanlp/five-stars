b59406137cfff4f0335a0de5fe8c39c6e18af51c
Roman Lapacherie <roman.lapacherie@gmail.com>
2023-04-25 18:07:30 +0100

    add links to watchlist and ratings in user menu

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,43 +1,50 @@
-import {Avatar, Group, Menu, Text, UnstyledButton} from '@mantine/core';
+import { Avatar, Group, Menu, Text, UnstyledButton } from '@mantine/core';
 import Link from 'next/link';
-import {useAuth} from "../../lib/firebase.auth";
+import { useAuth } from "../../lib/firebase.auth";
 import LoginModal from '../login-modal/LoginModal';
 import styles from './Header.module.css';
 
 
 export default function Header() {
-    const {user, signout} = useAuth();
+  const { user, signout } = useAuth();
 
 
-    return <header className={styles.header}>Five Stars
-        <nav className={styles.navitem}>
-            <Link href="/">Home</Link>
+  return <header className={styles.header}>Five Stars
+    <nav className={styles.navitem}>
+      <Link href="/">Home</Link>
 
-            {user ?
-                <Menu
-                    width={260}
-                    transition="pop-top-right"
-                >
-                    <Menu.Target>
-                        <UnstyledButton>
-                            <Group spacing={7}>
-                                <Avatar src={user.photoUrl} alt={user.email} radius="xl" size={25}/>
-                                <Text weight={500} size="sm" sx={{lineHeight: 1}} mr={3} color="#f57e7e">
-                                    {user.email}
-                                </Text>
-                            </Group>
-                        </UnstyledButton>
-                    </Menu.Target>
+      {user ?
+        <Menu
+          width={260}
+          transition="pop-top-right"
+        >
+          <Menu.Target>
+            <UnstyledButton>
+              <Group spacing={7}>
+                <Avatar src={user.photoUrl} alt={user.email} radius="xl" size={25}/>
+                <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3} color="#f57e7e">
+                  {user.email}
+                </Text>
+              </Group>
+            </UnstyledButton>
+          </Menu.Target>
 
-                    <Menu.Dropdown>
-                        <Menu.Item onClick={() => signout()}>
-                            Log out
-                        </Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
-                :
-                <LoginModal/>
-            }
-        </nav>
-    </header>
+          <Menu.Dropdown>
+            <Link href="/ratings">
+              <Menu.Item>My ratings</Menu.Item>
+            </Link>
+            <Link href="/watchlist">
+              <Menu.Item>Watchlist</Menu.Item>
+            </Link>
+            <Menu.Divider/>
+            <Menu.Item onClick={() => signout()}>
+              Log out
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+        :
+        <LoginModal/>
+      }
+    </nav>
+  </header>
 }
